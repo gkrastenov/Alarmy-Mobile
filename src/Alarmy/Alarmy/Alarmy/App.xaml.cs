7e2ffdfd0d05da3656ad57fac8549b1787549c67
haldenbg <georgi_kr1991@abv.bg>
2020-05-08 20:32:50 +0300

    Initial commit

---
+++ src/Alarmy/Alarmy/Alarmy/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Alarmy.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Alarmy
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            // TODO : Read more documentation about stack on pages and etc.....         
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            MainPage = new NavigationPage(new ListAlarmPages());
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ src/Alarmy/Alarmy/Alarmy/Pages/ListAlarmPages.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Alarmy.Pages
+{
+    [DesignTimeVisible(false)]
+    public partial class ListAlarmPages : ContentPage
+    {
+        public ListAlarmPages()
+        {
+            InitializeComponent();
+        }
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            await label.RelRotateTo(360, 1000);
+        }
+    }
+}
